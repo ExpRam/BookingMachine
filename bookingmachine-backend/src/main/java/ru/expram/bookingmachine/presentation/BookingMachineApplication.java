0926042b37b261a3b0420ccad4dbd1a3aa694a5d
ExpRam (superplays_) <80772324+ExpRam@users.noreply.github.com>
2025-04-16 21:20:45 +0300

    Cache Fix

--- bookingmachine-backend/src/main/java/ru/expram/bookingmachine/presentation/BookingMachineApplication.java
+++ bookingmachine-backend/src/main/java/ru/expram/bookingmachine/presentation/BookingMachineApplication.java
@@ -2,10 +2,8 @@ package ru.expram.bookingmachine.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class BookingMachineApplication {
 
 	public static void main(String[] args) {
---
+++ bookingmachine-backend/src/main/java/ru/expram/bookingmachine/presentation/configurations/CacheConfiguration.java
@@ -0,0 +1,28 @@
+package ru.expram.bookingmachine.presentation.configurations;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(2, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager caffeineCacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+
+}
